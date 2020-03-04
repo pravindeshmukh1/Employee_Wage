be3e42863581eec78070e90de68ff473a6c73819
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-04 17:10:46 +0530

    ADDED:Feature to Calculate Employee Daily Wage

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -x
 
+#CONSTANTS
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
+
+
 echo "Welcome to Employee Wage Computation"
 
 function check_Attendance() {
 	if [ $((RANDOM%2)) -eq 1 ]
 	then
 		echo "Employee Present"
+		dailyWage=$(($WAGE_PER_HOUR * $FULL_DAY_HOUR))
 	else
 		echo "Employee Absent"
 	fi
