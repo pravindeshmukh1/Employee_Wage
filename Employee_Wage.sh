8ad97675f7630c041e7ec95de4d7b708081f144a
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-04 16:51:06 +0530

    ADDED:Using RANDOM Check Employee is Present or Absent

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation"
+
+function check_Attendance() {
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		echo "Employee Present"
+	else
+		echo "Employee Absent"
+	fi
+}
+check_Attendance
