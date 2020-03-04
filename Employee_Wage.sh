79fa3e44f29a37459bf49a345bff583501366e78
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-04 17:47:20 +0530

    ADDED:Feature To Add Employee Part Time Employee and Calculate Emplyee Wage

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -2,18 +2,26 @@
 
 #CONSTANTS
 WAGE_PER_HOUR=20
-FULL_DAY_HOUR=8
-
+ISFUllTIME=1
+IsPARTTIME=2
 
 echo "Welcome to Employee Wage Computation"
 
 function check_Attendance() {
-	if [ $((RANDOM%2)) -eq 1 ]
+	if [ $((RANDOM%2)) -eq 0 ]
 	then
-		echo "Employee Present"
-		dailyWage=$(($WAGE_PER_HOUR * $FULL_DAY_HOUR))
-	else
 		echo "Employee Absent"
+		empWage=0
+	else
+		if [ $ISFUllTIME -eq $((RANDOM%2)) ]
+		then
+			empHours=8
+		elif [ $ISFULLTIME -eq $((RANDOM%2)) ]
+		then
+			empHours=4
+		fi
+	dailyWage=$(($WAGE_PER_HOUR*$empHours))
+	echo " Employee Daily Wage:"$dailyWage
 	fi
 }
 check_Attendance
