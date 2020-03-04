55af96fe9bde360ec8c0f7f14a1622530fc2d43b
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-04 18:48:05 +0530

    ADDED: Calculate Employee Month Wagge

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -3,17 +3,23 @@ echo "Welcome to Employee Wage Computation"
 
 #CONSTANTS
 WAGE_PER_HOUR=20
+WORKING_DAY=20
 IS_FUll_TIME=1
 IS_PART_TIME=2
 
 
 function getEmployeeWage() {
-	case $((RANDOM%3)) in
-	$IS_FULL_TIME ) empHours=8 ;;
+
+for ((d=1;d<=20 ;d++ ))
+do
+case $((RANDOM%3)) in
+	$IS_FUll_TIME ) emphours=8 ;;
 	$IS_PART_TIME ) empHours=4 ;;
 	* ) empHours=0 ;;
 	esac
 	employeeWage=$(($WAGE_PER_HOUR*$empHours))
-	echo "Employee Daily Wage:"$employeeWage
+	totalWage=$(($totalWage+$employeeWage))
+	echo "Employee Month Wage:"$employeeWage
+done
 }
 getEmployeeWage
