686f006283d7ae01284259c1f208f2e16c1c3135
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-04 19:23:03 +0530

    ADDED:Calculate Employee Wage Reached Till Hour and Day

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -6,20 +6,25 @@ WAGE_PER_HOUR=20
 WORKING_DAY=20
 IS_FUll_TIME=1
 IS_PART_TIME=2
+WORKING_HOUR=100
 
-
+day=0
+hour=0
 function getEmployeeWage() {
 
-for ((d=1;d<=20 ;d++ ))
+while [[ $day -lt $WORKING_DAY && $hour -lt $WORKING_HOUR ]]
 do
+((day++))
 case $((RANDOM%3)) in
-	$IS_FUll_TIME ) emphours=8 ;;
+	$IS_FUll_TIME ) empHours=8 ;;
 	$IS_PART_TIME ) empHours=4 ;;
 	* ) empHours=0 ;;
 	esac
+hour=$(($hour+$empHours))
+done
 	employeeWage=$(($WAGE_PER_HOUR*$empHours))
 	totalWage=$(($totalWage+$employeeWage))
-	echo "Employee Month Wage:"$employeeWage
-done
+	echo "Employee Wage:"$totalWage
+	echo "$hour: Hour $day: Day"
 }
 getEmployeeWage
